bbf514495e8e17e34bde472820bb9c93b79e0e47
Ewoma Lucky <ewomaluckyomogbolu@gmail.com>
2022-08-05 14:34:09 +0100

    My first commit

---
+++ FITNESS TEST/script.js
@@ -0,0 +1,85 @@
+const change = document.getElementById("brown");
+
+change.addEventListener("click", (event) => {
+    event.target.style.background = "brown";
+    event.target.style.color = "white";
+});
+
+const change2 = document.getElementById("face");
+
+change2.addEventListener("click", (event) => {
+    event.target.style.background = "brown";
+    event.target.style.color = "green";
+});
+
+const change3 = document.getElementById("time");
+
+change3.addEventListener("click", (event) => {
+    event.target.style.background = "green";
+    event.target.style.color = "white";
+});
+
+const change4 = document.getElementById("fish");
+
+change4.addEventListener("click", (event) => {
+    event.target.style.background = "black";
+    event.target.style.color = "white";
+});
+
+const change5 = document.getElementById("book");
+
+change5.addEventListener("click", (event) => {
+    event.target.style.background = "yellow";
+    event.target.style.color = "white";
+});
+
+const change6 = document.getElementById("hair");
+
+change6.addEventListener("click", (event) => {
+    event.target.style.background = "blue";
+    event.target.style.color = "white";
+});
+
+const change7 = document.getElementById("flower");
+
+change7.addEventListener("click", (event) => {
+    event.target.style.background = "green";
+    event.target.style.color = "white";
+});
+
+const change8 = document.getElementById("sleep");
+
+change8.addEventListener("click", (event) => {
+    event.target.style.background = "yellow";
+    event.target.style.color = "white";
+});
+
+const change9 = document.getElementById("run");
+
+change9.addEventListener("click", (event) => {
+    event.target.style.background = "red";
+    event.target.style.color = "white";
+});
+
+const change10 = document.getElementById("dance");
+
+change10.addEventListener("click", (event) => {
+    event.target.style.background = "blue";
+    event.target.style.color = "white";
+});
+
+const change11 = document.getElementById("power");
+
+change11.addEventListener("click", (event) => {
+    event.target.style.background = "brown";
+    event.target.style.color = "white";
+});
+
+const change12 = document.getElementById("strength");
+
+change12.addEventListener("click", (event) => {
+    event.target.style.background = "green";
+    event.target.style.color = "white";
+});
+
+
